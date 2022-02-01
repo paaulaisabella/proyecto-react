59f4e64cdb014148bf3574819d3408566d7be336
Paula <paulaisabellavasquez@gmail.com>
2022-01-31 23:54:23 -0300

    creé un navbar para el desafío, el css está específicado en la sección de App.css

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+let NavBar = () => {
+    return (
+        <>
+    <nav className= "navbar">
+        <h1 className="title">Shine Like a Diamond</h1>
+        <ul className="navlist">
+        <li className="navlist-item"><a className="navlist-link">Inicio</a></li>
+        <li className="navlist-item"><a className="navlist-link">Catálogo</a></li>
+        <li className="navlist-item"><a className="navlist-link">Contácto</a></li>
+        </ul>
+    </nav>
+    </>
+    );
+}
+
+export default NavBar;
